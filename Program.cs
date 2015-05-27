b5b457191a1eaf81001678075ff31360ae4ef59f
Victor Grigoriu <vgrigoriu@gmail.com>
2015-05-27 14:41:21 +0300

    First version

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace DisconnectingServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var port = args.Length > 0 ? int.Parse(args[0]) : 1234;
+            var socket = new TcpListener(new IPEndPoint(IPAddress.Any, port));
+            socket.Start();
+            while (true)
+            {
+                var client = socket.AcceptTcpClient();
+                var worker = new BackgroundWorker();
+                worker.DoWork += (sender, eventArgs) =>
+                {
+                    using (client)
+                    {
+                        var stream = client.GetStream();
+                        var reader = new StreamReader(stream);
+                        var line = reader.ReadLine();
+                        Console.WriteLine("Received: {0}", line);
+                    }
+                };
+                worker.RunWorkerAsync();
+            }
+        }
+    }
+}
